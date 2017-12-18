25e7bb0d46c0205ec6077823fdc3e35b1520a2de
govinduk <govindu.k@digitalfuel.com>
2017-12-18 18:19:52 +0530

    Initial commit

---
+++ src/main/java/com/springmvc/crud/SpringAngularjsCrud/SpringAngularjsCrudApplication.java
@@ -0,0 +1,12 @@
+package com.springmvc.crud.SpringAngularjsCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularjsCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngularjsCrudApplication.class, args);
+	}
+}
---
+++ src/test/java/com/springmvc/crud/SpringAngularjsCrud/SpringAngularjsCrudApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springmvc.crud.SpringAngularjsCrud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringAngularjsCrudApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
